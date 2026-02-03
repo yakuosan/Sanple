946a9643a7eb6c79b69f94d315f4ae6c4f855ebe
yakuosan <yakuo9359@gmail.com>
2026-02-03 14:15:18 +0900

    Initial Commit

---
+++ Sanple/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sanple
+//
+//  Created by 阪上八雲 on 2026/02/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sanple/SanpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SanpleApp.swift
+//  Sanple
+//
+//  Created by 阪上八雲 on 2026/02/03.
+//
+
+import SwiftUI
+
+@main
+struct SanpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
